4ccd0ea536e80cf98f6c771d0f07638860500a7b
Horia Manea <horiamanea.m@gmail.com>
2026-02-27 14:57:29 +0200

    [008] feat(): find all paragraphs in header and footer from a document

--- src/main.py
+++ src/main.py
@@ -32,6 +32,18 @@ def find_placeholders(document: Document):
     for table in document.tables:
         all_paragraphs.extend(extract_table_paragraphs(table))
 
+    for section in document.sections:
+        all_paragraphs.extend(section.header.paragraphs)
+        all_paragraphs.extend(section.footer.paragraphs)
+
+        for table in section.header.tables:
+            all_paragraphs.extend(extract_table_paragraphs(table))
+
+        for table in section.footer.tables:
+            all_paragraphs.extend(extract_table_paragraphs(table))
+        
+
+
     for i, paragraph in enumerate(document.paragraphs):
         matches = pattern.findall(paragraph.text)
         if matches:
